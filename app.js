3cf1c450909873d8206fe489fae2366ad3e84583
Dishant Sethi <dishantsethi@Dishants-MacBook-Air.local>
2021-08-25 00:36:54 +0530

    cors

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 // import express from 'express';
 // import dotenv from 'dotenv';
-const express=require('express');
-const dotenv=require('dotenv');
-const app=express();
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+const cors = require("cors");
 
-dotenv.config({path:'./config.env'});
+dotenv.config({ path: "./config.env" });
 
-const Connection=require('./db/conn.js');
-
-
+const Connection = require("./db/conn.js");
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use(require('./router/auth'));
+app.use(require("./router/auth"));
 
-
-
-
-const PORT=process.env.PORT;
-app.listen(PORT,()=>{
-    console.log(`listenning form ${PORT}`)
-})
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`listenning form ${PORT}`);
+});
 
 Connection();
--- model/userSchema.js
+++ model/userSchema.js
@@ -1,24 +1,24 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    phone:{
-        type:Number,
-        required:true
-    },
-    description:{
-        type:String,
-        required:true
-    }
-})
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
 
-const User=mongoose.model('User',userSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports=User;
+module.exports = User;
